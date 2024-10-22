dc18e9291e032e89e2fa75bcc427163665135dd3
thomasmclaughlin24 <88062019+thomasmclaughlin24@users.noreply.github.com>
2024-10-22 19:52:56 -0400

    this is my second addition to the repo, adding better functionality

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-def main():
-    print('Menu\n-------------\n'
-          '1. Encode\n2. Decode\n'
-          '3. Quit\n')
-    option = int(input('Please enter an option: '))
-    if option == 1:
-        password = str(input('Please enter your password to encode: '))
-        encoded_password = encode(password)
+# Name: Thomas McLaughlin
 
 def encode(password):
     encoded_key = ''
-    for i in range(len(password)):
-        altered_digit = int(password[i]) + 3
+    for i in range(len(password)
+	altered_digit = int(password[i] + 3
         encoded_key += str(altered_digit)
     return encoded_key
 
 if __name__ == '__main__':
-    main()
-
+    finished = False
+    while not finished:
+        print('\nMenu\n-------------\n'
+              '1. Encode\n2. Decode\n'
+              '3. Quit\n')
+        option = int(input('Please enter an option: '))
+        if option == 1:
+            password = str(input('Please enter your password to encode: '))
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            print("The encoded password is " + encoded_password + ", and the original password is " + password + ".")
+        elif option == 3:
+            finished = True
